8e25094dc6e9415693c0679c63f07b78f888c2ee
Adam Spiers <hackbrexit@adamspiers.org>
2018-08-08 22:32:18 +0100

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# seed_file = File.join(Rails.root, 'db', 'seeds.yml')
+# seeds = YAML::load_file(seed_file)
+# Question.create(config[:questions])
+
+immigration = Question.create(
+  title: "Immigration",
+  body: "Which of the following three is closest to how you feel about immigration?"
+)
